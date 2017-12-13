8578004cde21176f15c9b3cc06623f4f5cfee710
AlexBarett <iron.angel876527@gmail.com>
2017-12-13 19:27:04 +0500

    проверить

--- client-core.js
+++ client-core.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports.execute = execute;
-module.exports.isStar = true;
+module.exports.isStar = false;
 
 const chalk = require('chalk');
 const request = require('request');
